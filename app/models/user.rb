8224c2ea91e259711cf8ef0725befc3405a25137
Inframatic <isteffy@hotmail.com>
2013-07-03 16:09:07 -0400

    Creating a user model, controller and view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	attr_accesible :email, :password, :password_confirmation
-	has_secured_password
+	# attr_accesible :email, :password, :password_confirmation
+	# has_secured_password
 	validates_presence_of :password, :on => :create
 end
